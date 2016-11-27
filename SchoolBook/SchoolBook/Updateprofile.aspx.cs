2a4560c43e4721d53539557b95886848aa772f48
zackrobi <zacharyrobicheaux@gmail.com>
2016-11-27 12:28:03 -0600

    Update Updateprofile.aspx.cs

--- SchoolBook/SchoolBook/Updateprofile.aspx.cs
+++ SchoolBook/SchoolBook/Updateprofile.aspx.cs
@@ -12,7 +12,7 @@ namespace SchoolBook
     {
         string connstr = @"Data Source=localhost; Database=school_book_db; User Id=root; Password=password1";
 
-
+        //Fills all text boxes with user information
         protected void Page_Load(object sender, EventArgs e)
         {
             string email = Request.QueryString["Parameter"].ToString();
@@ -22,7 +22,9 @@ namespace SchoolBook
                 try
                 {
                     conn.Open();
-
+                    //Below code: 
+                    //Mysql commands to select the user information are set to strings cmd and cmd2. Said strings are made into sql commands.
+                    //These commands are now used to pull the user information and add said information to their respective text boxes.
                     string cmd = "SELECT PhoneNo FROM useraccounts where Email = '" + email + "';";
                     string cmd2 = "SELECT FullName FROM useraccounts where Email = '" + email + "';";
                     MySqlCommand sqlcmd = new MySqlCommand(cmd, conn);
@@ -43,16 +45,19 @@ namespace SchoolBook
         {
 
         }
-
+        //This method uses the text inside the text boxes to updats the user profile information inside the databse.
         protected void UpateProfileButton_Click(object sender, EventArgs e)
         {
-
+            //Creates a string value for phone number, name and email. Sets the phone number and name to the respective text box values.
+            //QueryString is used to get the user's email
             string PhoneNo = PhoneNumber.Text.ToString();
             string Name = FullName.Text.ToString();
             string email = Request.QueryString["Parameter"].ToString();
-            MySqlConnection conn = new MySqlConnection(connstr);
+            MySqlConnection conn = new MySqlConnection(connstr);    //Creates connection
 
-            //Add Validation Code
+            //Below code:
+            //A MySql Select command is set as a string. This command will update the phone number and full name to whatever strings are held in their respective text box.
+            //the command is then ran and a message will display telling the user if it succeeeded or failed.
             try
             {
                 conn.Open();
