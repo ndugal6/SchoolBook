b74ec5fb8d6cd294085b48852786f481253d40a4
Sean Marino <smarin9@lsu.edu>
2016-11-26 22:26:41 -0600

    Update Join Study Group - Sean Marino

--- SchoolBook/SchoolBook/Join Study Group.aspx.cs
+++ SchoolBook/SchoolBook/Join Study Group.aspx.cs
@@ -57,7 +57,7 @@ namespace SchoolBook
             }
             catch (MySqlException ex)
             {
-
+                Message.InnerHtml = ex.ToString();
             }
             conn.Close();
         }
