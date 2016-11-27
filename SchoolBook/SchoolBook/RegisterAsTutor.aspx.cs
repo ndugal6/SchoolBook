351a14004d23e60ee141e82fa84c772a8e18b910
zackrobi <zacharyrobicheaux@gmail.com>
2016-11-27 13:02:32 -0600

    Update RegisterAsTutor.aspx.cs

--- SchoolBook/SchoolBook/RegisterAsTutor.aspx.cs
+++ SchoolBook/SchoolBook/RegisterAsTutor.aspx.cs
@@ -18,6 +18,9 @@ namespace SchoolBook
     public partial class RegisterAsTutor : System.Web.UI.Page
     {
         string connstr = @"Data Source=localhost; Database=school_book_db; User Id=root; Password=password1";
+        
+        //Below code fills in the drop down list with all courses in the user's university.
+        //This is completed with a select sql command that selects all class names and course IDs and binds that data to the drop down list.
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Page.IsPostBack)
@@ -40,16 +43,21 @@ namespace SchoolBook
                 ddlCourses.Items.Insert(0, new ListItem("--Select Course--", "0"));
             }
         }
-
+        //Below code will take selected pdf file and class and creates a new row in the coursetaken table using all info gathered.
         protected void Register_Click(object sender, EventArgs e)
         {
-            string Email = Request.QueryString["Parameter"].ToString();
-
+            string Email = Request.QueryString["Parameter"].ToString(); //Gains user email by using QueryString
+            
+            //Finding file name and type of file that was uploaded
             string filename = Path.GetFileName(pdfResult.PostedFile.FileName);
             string contentType = pdfResult.PostedFile.ContentType;
 
+            //If it's a pdf, continue. If it isn't then throw an error
             if (contentType == "application/pdf")
-            {
+            {   
+                //Below code:
+                //Gets string value of the course taken ID of the user using a select command. 
+                //Gets the course ID and grade using the selected option on the drop down list.
                 MySqlConnection conn = new MySqlConnection(connstr);
                 conn.Open();
                 string selCourseTakenID = "select CourseTakenID from useraccounts where Email='" + Email + "';";
@@ -59,6 +67,9 @@ namespace SchoolBook
                 string courseID = ddlCourses.SelectedValue;
                 string grade = ddlGrade.SelectedValue;
 
+                //Below code:
+                //A new row is added into the coursetaken MySql table using the insert command.
+                //The row is filled with all string values created before said code.
                 using (Stream fs = pdfResult.PostedFile.InputStream)
                 {
                     using (BinaryReader br = new BinaryReader(fs))
